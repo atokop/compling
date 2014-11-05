7a8ce5385117bac87e7e533c6684846115638d79
kwamina <atokop@gmail.com>
2014-11-05 16:07:44 -0500

    Split YourBig files into sentences inside u7 directory

---
+++ u7/s.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import os
+direc = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "scripts/")
+sys.path.append(direc)
+
+from small_analysis import big_sents as sents
+print sents
+
+a = 0
+for sent in sents:
+    sentence = " ".join(sent)
+    name = "sent_" + str(a)
+    f = open(name, "w")
+    f.write(sentence)
+    a = a+1
+    print "%s written" % name
+
